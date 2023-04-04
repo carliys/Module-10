597a2bb2f15c1b122156b84b94d369551dbdd156
carliys <cflores1833@yahoo.com>
2023-04-04 11:48:08 -0700

    test.js

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,10 +1,10 @@
-const Employee = require ("../Employee");
+const Employee = require ('../lib/Employee');
 const employee = new Employee ("Angie", "1234567", "Angieliz@gmail.com");
 
-test('test if we can get the constructor values for the employee object', () => {
-    expect(employee.name).toBe('Angie');
-    expect(employee.id).toBe('1234567');
-    expect(employee.email).toBe('Angieliz@gmail.com');
+test('should create a new Employee instance with name, id, email, and role', () => {
+    expect(employee.name).toEqual('Angie');
+    expect(employee.id).toEqual('1234567');
+    expect(employee.email).toEqual('Angieliz@gmail.com');
 });
 
 test('test if we can get the name from the getName() method', () => {

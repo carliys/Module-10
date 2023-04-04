c6f85525aae2aadd9c7b4af920be090cc9241588
carliys <cflores1833@yahoo.com>
2023-04-04 10:02:04 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Module-10
+# Module-10 (OOP) Team Profile Generator
 Team Profile Generator
+
+## Table of Contents:
+1. [Description](#Description)
+2. [Installation](#Installation)
+3. [Usage](#Usage)
+4. [Licence](#Licence)
+5. [Test](#Test)
+6. [Questions](#Questions)
+
+  # Description
+  In this module challenge you will have to create Node.js command line that shows your teams info.
+  So that youll have quick access to thier github profiles and emails.
+
+  # Installation
+  The user must input the inquirer package.
+
+  # Usage
+  To use the Team Profile Generator you will need to run node index.js in the terminal.
+
+  # Licence
+  None
+
+  # Test
+  No test instructions at the moment.
+
+  # Questions
+  My GitHub is CARLIYS, and my Email is carlosvargas3200@gmail.com
+
---
+++ __test__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = rquire ("../Employee");
+const employee = new Employee ("Angie", "1234567", "Angieliz@gmail.com");
+
+test('test if we can get the constructor values for the employee object', () => {
+    expect(employee.name).toBe('Angie');
+    expect(employee.id).toBe('1234567');
+    expect(employee.email).toBe('Angieliz@gmail.com');
+});
+
+test('test if we can get the name from the getName() method', () => {
+    expect(employee.getName()).toBe('Angie');
+});
+
+test('test if we can get the id from the getId() method', () => {
+    expect(employee.getId()).toBe('1234567');
+});
+
+test('test if we can get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('Angieliz@gmail.com');
+});
+
+test('test if we can get the role from the getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
